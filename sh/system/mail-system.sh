a995ad00de98dbfffb74cf614d739abe8d562766
kaawsdev <kagalpan@kacpp.xyz>
2021-04-18 20:28:03 +0900

    複数のサービス起動できるようにsystemdのインストールと設定。
    Dockerfileにsystemdインストールと設定追加。
    設定は各サービスの.serviceファイル。
    docker-compose.ymlファイルの作成。
    systemdが使用できるようにprivileged: true(--privilegedオプションと同じ)の追加と
    volumsに/sys/fs/cgrpupを読み込み専用でマウントも追加。

--- sh/system/mail-system.sh
+++ sh/system/mail-system.sh
@@ -5,7 +5,13 @@ if [[ -r /usr/local/etc/usres.txt ]]; then
     /usr/local/sh/mail/users_add.sh /usr/local/etc/users.txt
 fi
 
-/usr/sbin/supervisord
+# supervisord
+# /usr/sbin/supervisord
 
-exit 0
+exec /sbin/init
+systemctl daemon-reload
+systemctl start opendkim
+systemctl start opendmarc
+systemctl start postfix
+
 
