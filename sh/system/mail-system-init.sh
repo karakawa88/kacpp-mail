4e509d216b5575d7df39f5d5419130ea2c2028f1
kaawsdev <kagalpan@kacpp.xyz>
2021-04-25 21:07:51 +0900

    clamav-milterのビルドとインストールをDockerfileに追加。
    clamav-milterのディレクトリ配置やmsmtpでウイルス発見時にはメールで知らせる
    シェルスクリプトの配置と送る時のメールの設定ファイル(.clamavmsmtprc)を配置。

--- sh/system/mail-system-init.sh
+++ sh/system/mail-system-init.sh
@@ -10,11 +10,13 @@ systemctl daemon-reload
 systemctl enable saslauthd
 systemctl enable opendkim
 systemctl enable opendmarc
+systemctl enable clamav-milter
 systemctl enable postfix
 
 systemctl start saslauthd
 systemctl start opendkim
 systemctl start opendmarc
+systemctl start clamav-milter
 systemctl start postfix
 
 exit 0
