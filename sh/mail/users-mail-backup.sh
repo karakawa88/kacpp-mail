a6b54349fc405aafff08044cffd72760b3ad0078
kaawsdev <kagalpan@kacpp.xyz>
2022-10-26 19:14:32 +0900

    ユーザーメールバックアップスクリプトのログ出力をsyslogにもするように変更。
    users-mail-backup.shにsyslogと標準出力に書き出す処理を追加。
    syslogと標準出力と標準エラー出力に書き出す。

--- sh/mail/users-mail-backup.sh
+++ sh/mail/users-mail-backup.sh
@@ -11,8 +11,11 @@
 #   4       tarでエラー
 #
 
+# 各ユーザーのHomeディレクトリがあるディレクトリ
 USERS_HOME_DIR="/home/mail_users"
+# バックアップ先ディレクトリ
 DEST="/dest"
+# バックアップファイル
 BACKUP_FILE="mail_users-backup.tar.xz"
 # tar コマンドのオプション
 # COMPRESS_COMMAND=${COMPRESS_COMMAND:-"xz -T 0"}
@@ -20,19 +23,50 @@ TAR_WARN_ERR_OPTIONS=" --warning=no-file-changed --warning=no-file-removed --war
 # TAR_OPTIONS=" -cJvf $BACKUP_FILE --use-compress-prog='$COMPRESS_COMMAND' $TAR_WARN_ERR_OPTIONS "
 TAR_OPTIONS=" -cJf $BACKUP_FILE  $TAR_WARN_ERR_OPTIONS "
 
+##
+# 概要: ログをsyslogと標準出力に書き出す。
+# 説明: 第一引数に指定したメッセージを標準出力とsyslogに書き出す。
+#       syslogのファシリティとプライオリティはmail.infoである。
+# 引数: $1  メッセージ
+function log_info() {
+    local mess = $1
+
+    logger -p mail.info -t MaildirBackup "$mess"
+    echo "$mess"
+    return 0
+}
+##
+# 概要: エラーログをsyslogと標準エラー出力に書き出す。
+# 説明: 第一引数に指定したメッセージを標準エラー出力とsyslogに書き出す。
+#       syslogのファシリティとプライオリティはmail.errである。
+# 引数: $1  メッセージ
+function log_err() {
+    local mess = $1
+
+    logger -p mail.err -t MaildirBackup "$mess"
+    echo "$mess" 1>&2
+    return 0
+}
+
 # ユーザーたちのHOMEディレクトリ
 if [[ ! -d "$USERS_HOME_DIR" ]]
 then
-    echo "ユーザーのHomeディレクトリが見つかりません。[$USERS_HOME_DIR]" 1>&2
+    log_err "ユーザーのHomeディレクトリが見つかりません。[$USERS_HOME_DIR]"
     exit 1
 fi
 # バックアップ出力先
 if [[ ! -d "$DEST" ]]
 then
-    echo "バックアップ先ディレクトリが見つかりません。[$DEST]" 1>&2
+    log_err "バックアップ先ディレクトリが見つかりません。[$DEST]"
     exit 2
 fi
 
+# バックアップするユーザーのメールのディレクトリ一覧の取得
+log_info "ユーザーのメールのバックアップ(Maildir)"
+log_info "バックアップ先ディレクトリ: $DEST"
+log_info "バックアップファイル: $BACKUP_FILE"
+
+# バックアップ元のリストスペース区切り
 SRC=""
 shopt -s lastpipe
 find "$USERS_HOME_DIR" -maxdepth 1 -type d -print |\
@@ -45,9 +79,11 @@ do
         continue
     fi
 done
-[[ -z "$SRC" ]] && echo "ユーザーのMaildirがひとつも見つかりません。" 1>&2 && exit 3
+[[ -z "$SRC" ]] && log_err "ユーザーのMaildirがひとつも見つかりません。" && exit 3
+log_info "バックアップするメール: $SRC"
 
 cd $DEST
+log_info "tarのオプション ${TAR_OPTIONS}"
 tar ${TAR_OPTIONS} $SRC
 ret=$?
 if (( $ret > 2 )) 
@@ -55,5 +91,6 @@ then
     echo "Error: tarバックアップエラー" 1>&2
     exit 4
 fi
+log_info "ユーザーメールのバックアップ完了"
 
 exit 0
